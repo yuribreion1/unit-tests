a919474f6bbaa16d927496d891b22dc38f89594f
Yuri Breion <yuribreion@br.ibm.com>
2021-11-28 22:58:28 -0300

    algumas validacoes

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -7,13 +7,21 @@ import java.util.Date;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.exception.FilmeSemEstoqueException;
+import br.ce.wcaquino.exception.LocadoraException;
 import br.ce.wcaquino.utils.DataUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class LocacaoService {
 
-	public Locacao alugarFilme(Usuario usuario, Filme filme) {
+	public Locacao alugarFilme(Usuario usuario, Filme filme) throws FilmeSemEstoqueException, LocadoraException {
+
+		if (usuario == null) throw new LocadoraException("Usuário vazio");
+		if (filme == null) throw new LocadoraException("Filme vazio");
+
+		if (filme.getEstoque() == 0 ) throw new FilmeSemEstoqueException();
+
 		Locacao locacao = new Locacao();
 		locacao.setFilme(filme);
 		locacao.setUsuario(usuario);
